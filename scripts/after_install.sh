935502b2c607c9c9158739a0e39a355cb13cbd37
lasantha96 <lsanjeewa947@gmail.com>
2022-07-30 23:23:58 +0400

    updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-isExistApp = `pgrep httpd`
-if [[ -n  $isExistApp ]]; then
-    service httpd stop        
-fi
-yum install -y httpd
 EC2_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 sed -i "s/is deployed/is deployed on $EC2_INSTANCE_ID in $EC2_AZ/g" /var/www/html/index.html
-chmod 664 /var/www/html/index.html
+chmod 777 /var/www/html/index.html
 service httpd start
